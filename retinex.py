ca7b154ee0bae9fb0ab3805e65ada48d68c1d459
ofgulban <farukgulban@gmail.com>
2017-01-24 11:58:45 +0100

    Docstring update.

--- retinex.py
+++ retinex.py
@@ -8,7 +8,34 @@ np.seterr(divide='ignore', invalid='ignore')
 
 
 def MultiScaleRetinex_3D(image, scales=[1, 10, 30]):
-    """Multi scale retinex function for mri data."""
+    u"""Multi scale retinex for 3 dimensional images.
+
+    Parameters
+    ----------
+    image : 3d numpy array
+        Input image/data/volume.
+
+    scales : list
+        Standard deviations for Gaussian kernels. For futher
+        information see [1]. More or less than 3 values can be given.
+        Disscussion of how to determine/optimize the scale values can
+        be found in [2].
+
+    Returns
+    -------
+    msr: 3d numpy array
+        Corrected image.
+
+
+    References:
+    [1] https://github.com/scipy/scipy/blob/v0.16.1/scipy/ndimage/filters.py#L251
+
+    [2] Jobson, D. J., Rahman, Z. U., & Woodell, G. A. (1997).
+    A multiscale retinex for bridging the gap between color images and
+    the human observation of scenes. IEEE Transactions on Image
+    Processing, 6(7), 965–976. DOI: 10.1109/83.597272
+
+    """
     start = time.time()
     print('applying multi-scale retinex filter...')
 
