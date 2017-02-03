e0121c376a061e907fecbbe1f8927059975b7a8e
ofgulban <farukgulban@gmail.com>
2017-02-04 00:21:09 +0100

    pypi stuff.

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='retinex_for_mri',
-      version='0.2.6',
+      version='0.2.7',
       description='Retinex algorithm for MRI data.',
       url='https://github.com/ofgulban/retinex_for_mri',
-      download_url='https://github.com/ofgulban/retinex_for_mri/releases/tag/0.2.6',
+      download_url='https://github.com/ofgulban/retinex_for_mri/tarball/0.2.7',
       author='Omer Faruk Gulban',
       author_email='faruk.gulban@maastrichtuniversity.nl',
       license='GNU Geneal Public License Version 3',
       packages=['retinex_for_mri'],
       install_requires=['numpy', 'scipy'],
+      keywords=['mri'],
       zip_safe=True)
