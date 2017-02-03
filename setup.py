2c67041b6d4f64c4e83692f5679961d0b4b23f8a
ofgulban <farukgulban@gmail.com>
2017-02-03 21:26:48 +0100

    Still learning pypi conda stuff.

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='retinex_for_mri',
-      version='0.2.0',
+      version='0.2.5',
       description='Retinex algorithm for MRI data.',
       url='https://github.com/ofgulban/retinex_for_mri',
-      download_url='https://github.com/ofgulban/retinex_for_mri/releases/tag/0.2.4',
+      download_url='https://github.com/ofgulban/retinex_for_mri/releases/tag/0.2.5',
       author='Omer Faruk Gulban',
       author_email='',
       license='GNU Geneal Public License Version 3',
       packages=['retinex_for_mri'],
-      install_requires=['numpy', 'scipy', 'nibabel'],
+      install_requires=['numpy', 'scipy'],
       zip_safe=False)
