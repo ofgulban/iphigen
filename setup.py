11efec9210e7b676d43b5ef6eef19b94132d5930
ofgulban <farukgulban@gmail.com>
2018-05-28 21:46:35 +0200

    version bump

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='retinex_for_mri',
-      version='0.2.9',
+      version='0.3.0',
       description='Retinex algorithm for MRI data.',
       url='https://github.com/ofgulban/retinex_for_mri',
-      download_url='https://github.com/ofgulban/retinex_for_mri/archive/0.2.9.tar.gz',
+      download_url='https://github.com/ofgulban/retinex_for_mri/archive/0.3.0.tar.gz',
       author='Omer Faruk Gulban',
       author_email='faruk.gulban@maastrichtuniversity.nl',
       license='GNU General Public License Version 3',
       packages=['retinex_for_mri'],
       install_requires=['numpy', 'scipy'],
-      keywords=['mri'],
+      keywords=['mri', 'retinex'],
       zip_safe=True)
